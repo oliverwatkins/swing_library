34da3f8ac9733f4d93ab9b1a8f32537eaf24c157
oliverwatkins <oliver.f.watkins@gmail.com>
2014-12-21 02:51:46 +0100

    cleaning up old code

--- src/main/java/com/gg/Showcase.java
+++ src/main/java/com/gg/Showcase.java
@@ -21,11 +21,6 @@ import com.gg.slider.SliderTester;
 public class Showcase extends JFrame {
 
 	public Showcase() throws Exception {
-
-		
-		
-		
-
 		
 		try {
 		    for (LookAndFeelInfo info : UIManager.getInstalledLookAndFeels()) {
@@ -75,96 +70,12 @@ public class Showcase extends JFrame {
 		tabbedPane.add("Slider", SliderTester.getPanel());
 
 		tabbedPane.add("Calculation", CalculationTester.getPanel());
+		
+		tabbedPane.add("Table Dialog", CalculationTester.getPanel());
+		
 
 		this.add(tabbedPane);
 
-		//
-		// JMenu menu = new JMenu("Booker");
-		//
-		// JMenuBar menuBar = new JMenuBar();
-		// menuBar.add(menu);
-		// this.setJMenuBar(menuBar);
-		//
-		//
-		// //setting up menu items
-		// JMenuItem sItem = new JMenuItem(searchAction);
-		// menu.add(sItem);
-		// JMenuItem bItem = new JMenuItem(bookAction);
-		// menu.add(bItem);
-		// menu.addSeparator();
-		// JMenuItem settingsItem = new JMenuItem(settingsAction);
-		// menu.add(settingsItem);
-		//
-		//
-		//
-		// JPanel panel = new JPanel(new BorderLayout());
-		//
-		//
-		//
-		// table = new BookingTable();
-		// table.setAutoCreateRowSorter(true); // auto sorting enabled
-		// table.setModel(model);
-		//
-		// BookingTableRenderer renderer = new BookingTableRenderer();
-		// for (int i = 0; i < table.getColumnCount(); i++) {
-		// table.getColumnModel().getColumn(i).setCellRenderer(renderer);
-		// }
-		// table.setDefaultRenderer(Object.class, renderer);
-		//
-		// JPanel bottomPanel = new JPanel();
-		//
-		// final JButton bookButton = new JButton("book");
-		// bookButton.setEnabled(false);
-		//
-		// final JButton deleteButton = new JButton("delete");
-		// deleteButton.setEnabled(false);
-		// deleteButton.setToolTipText("for testing purposes only");
-		//
-		// final JButton createButton = new JButton("create");
-		// createButton.setToolTipText("for testing purposes only");
-		//
-		// table.setSelectionMode(ListSelectionModel.SINGLE_SELECTION);
-		//
-		// table.getSelectionModel().addListSelectionListener(new
-		// ListSelectionListener(){
-		//
-		// public void valueChanged(ListSelectionEvent e) {
-		//
-		// if (table.getSelectionModel().isSelectionEmpty()){
-		// bookButton.setEnabled(false);
-		// deleteButton.setEnabled(false);
-		// }else{
-		// bookButton.setEnabled(true);
-		// deleteButton.setEnabled(true);
-		// }
-		// }
-		// });
-		//
-		// deleteButton.addActionListener(new DeleteAction(this));
-		// bookButton.addActionListener(bookAction);
-		// createButton.addActionListener(new CreateAction(this));
-		// searchPanel.getSearchButton().addActionListener(new
-		// SearchAction(this));
-		//
-		// bottomPanel.add(createButton);
-		// bottomPanel.add(bookButton);
-		// bottomPanel.add(deleteButton);
-		//
-		// JScrollPane scroll = new JScrollPane(table);
-		// scroll.setPreferredSize(new Dimension(800, 300));
-		//
-		// panel.add(searchPanel, BorderLayout.NORTH);
-		// panel.add(scroll, BorderLayout.CENTER);
-		// panel.add(bottomPanel, BorderLayout.SOUTH);
-		//
-		// this.getContentPane().add(panel);
-		//
-
-		//
-		// this.pack();
-		// Utils.center(this);
-		//
-		// this.setVisible(true);
 	}
 
 	/**
